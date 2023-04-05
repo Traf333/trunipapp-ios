32323dff01e4c2728c20dc2cb328e640b4c2408b
Igor Trofimov <igor.trofimov@symmetre.com>
2023-04-05 11:12:32 +0200

    Initial Commit

---
+++ turnipsapp/turnipsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  turnipsappApp.swift
+//  turnipsapp
+//
+//  Created by Igor Trofimov on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct turnipsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
